719670492aacf92e5df13659f8ea2c5e5868070c
Simon Seyock <simonseyock@gmail.com>
2016-08-25 15:40:18 +0200

    V1.0.6 (#20)
    * Commented build.js
    
    * 1.0.6

--- build.js
+++ build.js
@@ -15,9 +15,9 @@ const selectConfig = require('./selectConfig')
 const webpackMerge = require('webpack-merge')
 
 let args = getopt.create([
-  ['m', 'mode=', 'The mode of the application. Either "dev" or "prod".'],
-  ['c', 'conf=', 'The config directory.'],
-  ['p', 'port=', 'The port to use. Defaults to 8080.']
+  [ 'm', 'mode=', 'The mode of the application. Either "dev" or "prod".' ],
+  [ 'c', 'conf=', 'The config directory.' ],
+  [ 'p', 'port=', 'The port to use. Defaults to 8080.' ]
 ])
   .bindHelp()
   .parseSystem()
@@ -30,25 +30,41 @@ if (!args.options.hasOwnProperty('conf')) {
   throw new Error('Option "conf" is required.')
 }
 
-let baseDir = process.cwd()
-let configPath = path.join(baseDir, args.options.conf)
+const baseDir = process.cwd()
+const configPath = path.join(baseDir, args.options.conf)
+
+// get the webpack.js file from the folder
 let buildConf = require(path.join(configPath, 'webpack.js'))
 
 if (args.options.mode === 'dev') {
-  buildConf = webpackMerge(buildConf, require('guide4you-builder/webpack.dev.js'))
+  // choose dev config wherever possible
   buildConf = selectConfig(buildConf, 'dev')
+  // merge with base dev config
+  buildConf = webpackMerge(buildConf, require('guide4you-builder/webpack.dev.js'))
+  // take out the devServer config
   let serverConf = buildConf.devServer
   delete buildConf.devServer
+  // configure dev proxy
   serverConf.proxy = devProxyConfig(buildConf)
+  // take port out of arguments
   let port = parseInt(args.options.port) || 8080
+  // set proper public path
   buildConf.output.publicPath = `http://localhost:${port}/`
+  // compile
   let compiler = webpack(buildConf)
+  // start server
   let server = new WebpackDevServer(compiler, serverConf)
   server.listen(port)
 } else if (args.options.mode === 'prod') {
-  buildConf = webpackMerge(buildConf, require('guide4you-builder/webpack.prod.js'))
+  // choose prod config wherever possible
   buildConf = selectConfig(buildConf, 'prod')
-  rimraf.sync(path.join(baseDir, buildConf.output.path))
+  // merge with base prod config
+  buildConf = webpackMerge(buildConf, require('guide4you-builder/webpack.prod.js'))
+  if (!buildConf.output.merge) {
+    // delete old build
+    rimraf.sync(buildConf.output.path)
+  }
+  // compile
   let compiler = webpack(buildConf)
   compiler.run((err, stats) => {
     if (err) {
