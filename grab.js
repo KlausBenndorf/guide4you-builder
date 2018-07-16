2044ba788e1759fbdce31bb0afd94ae1ea443251
Simon Seyock <simonseyock@gmail.com>
2018-07-16 17:07:03 +0200

    added watch and remove options to grab.js

--- grab.js
+++ grab.js
@@ -4,26 +4,87 @@
 
 const fs = require('fs')
 const path = require('path')
+const watch = require('watch')
 const copy = require('ncp').ncp
+const rimraf = require('rimraf')
+const Getopt = require('node-getopt')
 
 const curDir = process.cwd()
 
-for (let i = 2; i < process.argv.length; i++) {
-  let reponame = process.argv[i]
-  console.log('copying ' + reponame)
+const getopt = new Getopt([
+  [ 'w', 'watch', 'Watch the files for changes.' ],
+  [ 'r', 'remove', 'Remove the files for clean reinstall.' ]
+])
+
+getopt.parseSystem()
+
+if (getopt.argv.length < 1) {
+  getopt.setHelp(
+    'Usage: node grab.js repository [repository+] [OPTION]\n' +
+    'grabs all files but the node_modules and files starting with a ".".\n' +
+    '\n' +
+    '[[OPTIONS]]'
+  )
+  getopt.showHelp()
+  process.exit(0)
+}
+
+function removeStatusFunc (name) {
+  return function (err) {
+    if (err) {
+      console.error('Error removing ' + name)
+      console.error(err)
+    } else {
+      console.log('Done removing ' + name)
+    }
+  }
+}
+
+function copyStatusFunc (name) {
+  return function (err) {
+    if (err) {
+      console.error('Error copying ' + name)
+      console.error(err)
+    } else {
+      console.log('Done copying ' + name)
+    }
+  }
+}
+
+for (let reponame of getopt.argv) {
   let nodeModulePath = path.join(curDir, 'node_modules', reponame)
   let sourceRepoPath = path.join(curDir, '..', reponame)
   if (!fs.existsSync(nodeModulePath)) {
-    throw new Error('please install package before grabbing')
+    throw new Error('please install package once before grabbing to ensure that all dependencies are satisfied')
   }
 
-  copy(sourceRepoPath, nodeModulePath, {
-    filter: new RegExp(reponame + '.(?!node_modules)[^.]'),
-    stopOnError: true
-  }, function (err) {
-    if (err) {
-      console.error(err)
+  if (!getopt.options.remove) {
+    console.log('copying ' + reponame)
+    const filterRegEx = new RegExp(reponame + '(.(?!node_modules)[^.]|$)')
+    copy(sourceRepoPath, nodeModulePath, {
+      filter: filterRegEx,
+      stopOnError: true
+    }, copyStatusFunc(reponame))
+    if (getopt.options.watch) {
+      watch.watchTree(sourceRepoPath, {
+        filter: function (name) {
+          return filterRegEx.test(name)
+        }
+      }, function (f, curr, prev) {
+        if (typeof f === 'object') {
+          console.log('watching ' + reponame)
+        } else {
+          const t = path.join(nodeModulePath, path.relative(sourceRepoPath, f))
+          if (curr.nlink === 0) {
+            rimraf(t, removeStatusFunc(f))
+          } else {
+            copy(f, t, copyStatusFunc(f))
+          }
+        }
+      })
     }
-    console.log('done!')
-  })
+  } else {
+    console.log('removing ' + reponame)
+    rimraf(nodeModulePath, removeStatusFunc(reponame))
+  }
 }
